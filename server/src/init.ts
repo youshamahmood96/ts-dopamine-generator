c2357f7752fecdff7d34cc6a00b7b0d550b3a8b5
youshamahmood <youshamahmood96@gmail.com>
2021-10-21 22:18:25 +0600

    cors integrated

--- server/src/init.ts
+++ server/src/init.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import router from "./Routes/user.route";
-
+import cors from 'cors'
 const init = async():Promise<void> =>{
     const app = express();
+    app.use(cors());
     app.use(express.json());
     await app.use('/api/v1',router)
     await app.listen(process.env.PORT,()=>{
